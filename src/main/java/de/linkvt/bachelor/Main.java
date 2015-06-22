b2a81f7210be97448ad8c32c510ca74aee512577
Vincent Link <mail@linkvt.de>
2015-06-22 17:40:41 +0200

    Add basic project

---
+++ src/main/java/de/linkvt/bachelor/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Main.java
+ *
+ */
+
+package de.linkvt.bachelor;
+
+/**
+ * The entry point of the application.
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Application started.");
+	}
+}
