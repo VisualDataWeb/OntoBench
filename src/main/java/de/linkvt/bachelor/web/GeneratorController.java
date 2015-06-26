d62cfcca036f943aec9e2728f014590b27728e0c
Vincent Link <mail@linkvt.de>
2015-06-26 22:00:50 +0200

    Set text/plain as default content type

--- src/main/java/de/linkvt/bachelor/web/GeneratorController.java
+++ src/main/java/de/linkvt/bachelor/web/GeneratorController.java
@@ -5,12 +5,13 @@ import de.linkvt.bachelor.generator.OntologyGenerator;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController public class GeneratorController {
 
-	@RequestMapping("/ontology") public String ontology() {
+	@RequestMapping(value = "/ontology", produces = MediaType.TEXT_PLAIN_VALUE) public String ontology() {
 		String ontologyAsText = null;
 
 		try {
