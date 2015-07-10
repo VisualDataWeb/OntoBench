f87e380d42f4a93da60ee4ee5f91d96a004cd1df
Vincent Link <mail@linkvt.de>
2015-07-10 16:23:50 +0200

    Give equivalent property two equivalents

--- src/main/java/de/linkvt/bachelor/features/properties/OwlEquivalentPropertyFeature.java
+++ src/main/java/de/linkvt/bachelor/features/properties/OwlEquivalentPropertyFeature.java
@@ -3,16 +3,11 @@ package de.linkvt.bachelor.features.properties;
 import de.linkvt.bachelor.features.Feature;
 import de.linkvt.bachelor.generator.FeaturePool;
 
-import org.semanticweb.owlapi.model.OWLAnnotation;
-import org.semanticweb.owlapi.model.OWLAnnotationProperty;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLFunctionalObjectPropertyAxiom;
 import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLOntology;
-
-import java.util.Collections;
 
 /**
  * An owl equivalent property.
@@ -20,18 +15,20 @@ import java.util.Collections;
 public class OwlEquivalentPropertyFeature extends Feature {
   @Override
   protected void addTo(OWLOntology ontology, OWLDataFactory factory, FeaturePool featurePool) {
-    OWLObjectProperty property = featurePool.getReusableProperty();
-    featurePool.removePropertyFromPool(property);
-    OWLObjectProperty equivalentProperty = featurePool.getExclusiveProperty("EquivalentProperty");
+    OWLObjectProperty property = featurePool.getExclusiveProperty("EquivalentPropertyBase");
+    OWLObjectProperty equivalentProperty1 = featurePool.getExclusiveProperty("EquivalentProperty1");
+    OWLObjectProperty equivalentProperty2 = featurePool.getExclusiveProperty("EquivalentProperty2");
 
-    OWLAxiom equivalentAxiom = factory.getOWLEquivalentObjectPropertiesAxiom(property, equivalentProperty);
+    OWLAxiom equivalent1Axiom = factory.getOWLEquivalentObjectPropertiesAxiom(property, equivalentProperty1);
+    OWLAxiom equivalent2Axiom = factory.getOWLEquivalentObjectPropertiesAxiom(property, equivalentProperty2);
 
     OWLClass equivalentDomain = featurePool.getReusableClass();
     OWLClass equivalentRange = featurePool.getExclusiveClass("EquivalentPropertyTest");
-    OWLAxiom domainAxiom = factory.getOWLObjectPropertyDomainAxiom(equivalentProperty, equivalentDomain);
-    OWLAxiom rangeAxiom = factory.getOWLObjectPropertyRangeAxiom(equivalentProperty, equivalentRange);
+    OWLAxiom domainAxiom = factory.getOWLObjectPropertyDomainAxiom(property, equivalentDomain);
+    OWLAxiom rangeAxiom = factory.getOWLObjectPropertyRangeAxiom(property, equivalentRange);
 
-    addToOntology(ontology, equivalentAxiom);
+    addToOntology(ontology, equivalent1Axiom);
+    addToOntology(ontology, equivalent2Axiom);
     addToOntology(ontology, domainAxiom);
     addToOntology(ontology, rangeAxiom);
   }
