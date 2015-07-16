ab1d535e357447eb9f40e875bf3134d85700b4b9
Vincent Link <mail@linkvt.de>
2015-07-17 01:15:46 +0200

    Add temporary ontology iri

--- src/main/java/de/linkvt/bachelor/config/OwlApiConfig.java
+++ src/main/java/de/linkvt/bachelor/config/OwlApiConfig.java
@@ -1,9 +1,11 @@
 package de.linkvt.bachelor.config;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLMutableOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -15,6 +17,8 @@ import org.springframework.web.context.WebApplicationContext;
  */
 @Configuration
 public class OwlApiConfig {
+  private static final String DEFAULT_PREFIX = "http://linkvt.de/bachelor/";
+
   @Bean
   public OWLDataFactory dataFactory() {
     return OWLManager.getOWLDataFactory();
@@ -23,7 +27,8 @@ public class OwlApiConfig {
   @Bean
   @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.INTERFACES)
   public OWLMutableOntology owlOntology() throws OWLOntologyCreationException {
+    OWLOntologyManager ontologyManager = OWLManager.createOWLOntologyManager();
     // Cast to a mutable ontology to pass OWLApi's strange checks
-    return (OWLMutableOntology) OWLManager.createOWLOntologyManager().createOntology();
+    return (OWLMutableOntology) ontologyManager.createOntology(IRI.create(DEFAULT_PREFIX));
   }
 }
