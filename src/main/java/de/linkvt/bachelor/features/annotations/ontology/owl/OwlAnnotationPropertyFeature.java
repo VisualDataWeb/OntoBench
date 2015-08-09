7b07b38acf0d0e202276ec7679ac980191268c96
Vincent Link <mail@linkvt.de>
2015-08-09 23:18:40 +0200

    Improve owl annotation property

--- src/main/java/de/linkvt/bachelor/features/annotations/ontology/owl/OwlAnnotationPropertyFeature.java
+++ src/main/java/de/linkvt/bachelor/features/annotations/ontology/owl/OwlAnnotationPropertyFeature.java
@@ -2,25 +2,28 @@ package de.linkvt.bachelor.features.annotations.ontology.owl;
 
 import de.linkvt.bachelor.features.Feature;
 import de.linkvt.bachelor.features.FeatureCategory;
-import de.linkvt.bachelor.features.annotations.ontology.OntologyConstants;
 
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAnnotation;
 import org.semanticweb.owlapi.model.OWLAnnotationProperty;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLLiteral;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OwlAnnotationPropertyFeature extends Feature {
-
-  @Autowired
-  OntologyConstants ontologyConstants;
 
   @Override
   public void addToOntology() {
     OWLAnnotationProperty annotationProperty = factory.getOWLAnnotationProperty(IRI.create("customAnnotationProperty"));
     addAxiomToOntology(factory.getOWLDeclarationAxiom(annotationProperty));
 
+    OWLLiteral annotationValue = factory.getOWLLiteral("Custom Annotation Value");
+    OWLAnnotation annotation = factory.getOWLAnnotation(annotationProperty, annotationValue);
 
+    OWLClass owlClass = featurePool.getReusableClass();
+    addAxiomToOntology(factory.getOWLDeclarationAxiom(owlClass));
+    addAxiomToOntology(factory.getOWLAnnotationAssertionAxiom(owlClass.getIRI(), annotation));
   }
 
   @Override
